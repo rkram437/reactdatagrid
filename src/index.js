1da62086e317ab97b8c86a5674b88be6daf135ac
Ramesh V <ramesh.varadachari@7-11.com>
2020-07-22 12:55:43 -0500

    changes

---
+++ src/index.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ReactDataGrid from "react-data-grid";
+import { Toolbar, Data } from "react-data-grid-addons";
+import createRowData from "./createRowData";
+
+const defaultColumnProperties = {
+  filterable: true,
+  width: 120
+};
+
+const selectors = Data.Selectors;
+
+const columns = [
+  {
+    key: "id",
+    name: "ID"
+  },
+  {
+    key: "title",
+    name: "Title"
+  },
+  {
+    key: "firstName",
+    name: "First Name"
+  },
+  {
+    key: "lastName",
+    name: "Last Name"
+  },
+  {
+    key: "email",
+    name: "Email"
+  },
+  {
+    key: "street",
+    name: "Street"
+  },
+  {
+    key: "zipCode",
+    name: "ZipCode"
+  },
+  {
+    key: "date",
+    name: "Date"
+  },
+  {
+    key: "jobTitle",
+    name: "Job Title"
+  },
+  {
+    key: "catchPhrase",
+    name: "Catch Phrase"
+  },
+  {
+    key: "jobArea",
+    name: "Job Area"
+  },
+  {
+    key: "jobType",
+    name: "Job Type"
+  }
+].map(c => ({ ...c, ...defaultColumnProperties }));
+
+const ROW_COUNT = 50;
+
+const handleFilterChange = filter => filters => {
+  const newFilters = { ...filters };
+  if (filter.filterTerm) {
+    newFilters[filter.column.key] = filter;
+  } else {
+    delete newFilters[filter.column.key];
+  }
+  return newFilters;
+};
+
+function getRows(rows, filters) {
+  return selectors.getRows({ rows, filters });
+}
+
+function Example({ rows }) {
+  const [filters, setFilters] = useState({});
+  const filteredRows = getRows(rows, filters);
+  return (
+    <ReactDataGrid
+      columns={columns}
+      rowGetter={i => filteredRows[i]}
+      rowsCount={filteredRows.length}
+      minHeight={500}
+      toolbar={<Toolbar enableFilter={true} />}
+      onAddFilter={filter => setFilters(handleFilterChange(filter))}
+      onClearFilters={() => setFilters({})}
+    />
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Example rows={createRowData(50)} />, rootElement);
